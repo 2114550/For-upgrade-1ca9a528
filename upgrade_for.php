9e8dd1235ad86bf683494b5154cfda4122fd608c
2114550 <2114550@talnet.nl>
2019-10-03 09:40:51 +0200

    Uitwerking van de exercise

---
+++ upgrade_for.php
@@ -0,0 +1,12 @@
+<?php
+$klasgenoten = array(
+    "Sjonnie de Wiel".PHP_EOL,
+    "Herman Kaal".PHP_EOL,
+    "Henk de Steen".PHP_EOL,
+    "Inge Kerkhoven".PHP_EOL,
+    "Gert Kruiswijk",
+);
+for ($i = 0; $i < count($klasgenoten); $i++)
+{
+    echo $klasgenoten[$i];
+}
